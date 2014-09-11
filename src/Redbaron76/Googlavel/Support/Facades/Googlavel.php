07b71787cbdd88bcc9caa4ab44cf2f8e8d4b01f0
redbaron76 <f.fumis@gmail.com>
2014-09-11 09:16:51 +0200

    Editing Facade

---
+++ src/Redbaron76/Googlavel/Support/Facades/Googlavel.php
@@ -0,0 +1,14 @@
+<?php namespace Redbaron76\Googlavel\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Googlavel extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'Googlavel'; }
+
+}
