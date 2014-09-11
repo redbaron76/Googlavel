c971bf3a636054e131b6c49873870f37cdf25698
redbaron76 <f.fumis@gmail.com>
2014-09-11 08:53:19 +0200

    Init Googlavel package

---
+++ src/Redbaron76/Googlavel/GooglavelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Redbaron76\Googlavel;
+
+use Illuminate\Support\ServiceProvider;
+
+class GooglavelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('redbaron76/googlavel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    // OAuth2 Setting, you can get these keys in Google Developers Console
+    'oauth2_client_id'      => '< YOUR CLIENT ID >',
+    'oauth2_client_secret'  => '< YOUR CLIENT SECRET >',
+    'oauth2_redirect_uri'   => 'http://localhost:8000/',   // Change it according to your needs
+
+    // Definition of service specific values like scopes, OAuth token URLs, etc
+    'services' => array(
+
+        'calendar' => array(
+            'scope' => 'https://www.googleapis.com/auth/calendar'
+        ),
+        /*'books' => [
+            'scope' => 'https://www.googleapis.com/auth/books'
+        ]*/
+
+    ),
+
+    // Service file name prefix
+    'service_class_prefix' => 'Google_Service_',
+
+    // Custom settings
+    'access_type' => 'online',    
+    'approval_prompt' => 'auto',
+
+];
